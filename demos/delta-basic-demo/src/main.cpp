acbd1a6fd70ac78b6d601393a42997718026ca93
LukasACH <git@lukasach.dev>
2022-08-21 19:18:44 +0200

    Make delta-basic-demo compile on anything other than Windows

--- demos/delta-basic-demo/src/main.cpp
+++ demos/delta-basic-demo/src/main.cpp
@@ -1,7 +1,22 @@
+#ifdef __WIN32
 #include <Windows.h>
+#endif
 
 #include "../include/demo_list.h"
 
+
+void runApp(void *instance, ysContextObject::DeviceAPI api) {
+    delta_demo::DemoApplication::Demo demo =
+            delta_demo::DemoApplication::Demo::DeltaBasicDemo;
+
+    delta_demo::DemoApplication *app = delta_demo::CreateApplication(demo);
+    app->Initialize(instance, api);
+    app->Run();
+    app->Destroy();
+}
+
+
+#ifdef __WIN32
 int WINAPI WinMain(
     _In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -13,13 +28,19 @@ int WINAPI WinMain(
     (void)hPrevInstance;
 
     ysContextObject::DeviceAPI api = ysContextObject::DeviceAPI::DirectX11;
-    delta_demo::DemoApplication::Demo demo =
-        delta_demo::DemoApplication::Demo::DeltaBasicDemo;
-
-    delta_demo::DemoApplication *app = delta_demo::CreateApplication(demo);
-    app->Initialize((void *)&hInstance, api); 
-    app->Run();
-    app->Destroy();
+    runApp((void *)&hInstance, api);
 
     return 0;
 }
+#else
+
+int main() {
+
+    ysContextObject::DeviceAPI api = ysContextObject::DeviceAPI::OpenGL4_0;
+
+    runApp(nullptr, api);
+
+    return EXIT_SUCCESS;
+}
+
+#endif
